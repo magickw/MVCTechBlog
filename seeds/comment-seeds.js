1fedeb5738dad74c9988161686daa06e694f947c
Baofeng Guo <peterguo1983@gmail.com>
2022-02-14 08:38:18 -0800

    added ids to comments.

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,31 +2,37 @@ const { Comment } = require('../models');
 
 const commentData = [
   {
+    id: 1,
     comment_text: "Wow! I didn't know about datatables. Thanks!",
     post_id: 1,
     user_id: 4
   },
   {
+    id: 2,
     comment_text: "This is really helpful. Datatables makes my html tables beautiful",
     post_id: 1,
     user_id: 2
   },
   {
+    id: 3,
     comment_text: "Thank you for your content!",
     post_id: 2,
     user_id: 3
   },
   {
+    id: 4,
     comment_text: "Great. Thank you for your comparisons. They look similar to me.",
     post_id: 3,
     user_id: 2
   },
   {
+    id: 5,
     comment_text: "Handlebars have saved me a lot of time! Great.",
     post_id: 3,
     user_id: 4
   },
   {
+    id: 6,
     comment_text: "What is hashing in cyber security? I am really curious.",
     post_id: 4,
     user_id: 2
