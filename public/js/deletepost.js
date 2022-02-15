01a40080f34d195f6f1108d4997e60dd73f51e9d
Baofeng Guo <peterguo1983@gmail.com>
2022-02-14 16:06:26 -0800

    added the edit post handlebars.

---
+++ public/js/deletepost.js
@@ -0,0 +1,29 @@
+// Delete form handler
+async function deleteFormHandler(event) {
+  event.preventDefault();
+  
+  const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+
+    if(id){
+      const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+      body: JSON.stringify({
+        post_id: id
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+    // otherwise, display the error
+  } else {
+    alert(response.statusText);
+  }
+}
+}
+
+document.querySelector('#delete-btn').addEventListener('click', deleteFormHandler);
