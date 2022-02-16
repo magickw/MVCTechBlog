23022d961877a5421aae9b0761e9fe0191c10437
Baofeng Guo <peterguo1983@gmail.com>
2022-02-15 16:28:43 -0800

    updated edit post.js

--- public/js/editpost.js
+++ public/js/editpost.js
@@ -2,7 +2,9 @@
 
 async function editFormHandler (event) {
   event.preventDefault();
-  const id = document.querySelector('input[name="post-id"]').value.trim();
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
   const title = document.querySelector('input[name="post-title"]').value.trim();
   const post_text = document.querySelector('textarea[name="post-text"]').value.trim();
 
